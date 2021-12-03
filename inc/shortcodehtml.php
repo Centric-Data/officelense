0b3a73e82e856aadb5136d0e427ec3e5dd7d6d26
gillmourtunhira <gillmourtunhira@gmail.com>
2021-12-03 16:13:03 +0200

    offices details updated

--- inc/shortcodehtml.php
+++ inc/shortcodehtml.php
@@ -1,57 +1,57 @@
 <section>
- <div class="branch__wrapper lense-row">
-   <div class="branch__offices">
-     <h3>Provincial Offices</h3>
-     <ul>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Mashonaland Central Province</h4>
-           <p>Veterinary Complex<br>Bindura<br> info@zlc.co.zw <br> Tel: +263-266-2106003</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Manicaland Province</h4>
-           <p>Government Complex, 2nd Floor<br>Mutare<br> info@zlc.co.zw <br> Tel: +263-020-2061307</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Masvingo Province</h4>
-           <p>Block No.4, Old Government Complex<br>Masvingo<br> info@zlc.co.zw <br> Tel: +263-239-2260744</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Mashonaland East Province</h4>
-           <p>DA`s Office, Cnr 1st & S. Machel Street<br>Marondera<br> info@zlc.co.zw <br> Tel: +263-279-2325537</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Mashonaland West Province</h4>
-           <p>Former PA Building, Opposite OK Supermarket<br>Chinhoyi<br> info@zlc.co.zw <br> Tel: +263-0267-2123416</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Matebeleland North Province</h4>
-           <p>Government Complex (Social Services Building)<br>Cnr Fort Street & 10th Avenue<br>Bulawayo<br> info@zlc.co.zw <br> Tel: +263-029-63669</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Matebeleland South Province</h4>
-           <p>Mtshabezi Building<br>Cnr 4th Avenue & King Street<br>Gwanda<br> info@zlc.co.zw <br> Tel: +263-0284-2821178</p>
-         </div>
-       </li>
-       <li>
-         <div class="branch__offices--a">
-           <h4>Midlands Province</h4>
-           <p>New Government Complex<br>Gweru<br> info@zlc.co.zw <br> Tel: +263-254-2106003</p>
-         </div>
-       </li>
-     </ul>
-   </div>
- </div>
-</section>
+    	<div class="branch__wrapper lense-row">
+    		<div class="branch__offices">
+    			<h3>Provincial Offices</h3>
+    			<ul>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Mashonaland Central Province</h4>
+    						<p>Veterinary Complex<br>Bindura<br> info@zlc.co.zw <br> Tel: +263-266-2106003</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Manicaland Province</h4>
+    						<p>Government Complex, 2nd Floor<br>Mutare<br> info@zlc.co.zw <br> Tel: +263-020-2061307</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Masvingo Province</h4>
+    						<p>3rd Floor<br> Western Wing, ZESA Centre<br>Corner Robert Mugabe & Josiah Tongogara Street<br>Masvingo<br> info@zlc.co.zw <br> Tel: +263-239-2260744</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Mashonaland East Province</h4>
+    						<p>52 Ash Street <br>The Green, ZB Bank Complex<br>Marondera<br> info@zlc.co.zw <br> Tel: +263-279-2325537</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Mashonaland West Province</h4>
+    						<p>Former PA Building, Opposite OK Supermarket<br>Chinhoyi<br> info@zlc.co.zw <br> Tel: +263-0267-2123416</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Matebeleland North Province</h4>
+    						<p>Government Complex (Social Services Building)<br>Cnr Fort Street & 10th Avenue<br>Bulawayo<br> info@zlc.co.zw <br> Tel: +263-029-63669</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Matebeleland South Province</h4>
+    						<p>Mtshabezi Building<br>Cnr 4th Avenue & King Street<br>Gwanda<br> info@zlc.co.zw <br> Tel: +263-0284-2821178</p>
+    					</div>
+    				</li>
+    				<li>
+    					<div class="branch__offices--a">
+    						<h4>Midlands Province</h4>
+    						<p>Ground Floor <br>Office Number G001-G004,<br> New Government Complex <br>11th Street<br>Gweru<br> info@zlc.co.zw <br> Tel: +263-254-2106003</p>
+    					</div>
+    				</li>
+    			</ul>
+    		</div>
+    	</div>
+    </section>
